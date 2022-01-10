4f946d1ba58ba140229f9d2cf387b697d471bab5
Michel <59454429+ducter-dev@users.noreply.github.com>
2022-01-10 16:23:57 -0600

    :sparkles: Se agregaron las fuentes oficiales. (#8)

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="bg-white text-dark">
-    <h1 class="text-3xl font-bold underline">
+    <h1 class="text-3xl font-semibold">
       Hello world - SCADA v2!
     </h1>
   </div>
