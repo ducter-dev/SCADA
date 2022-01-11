a9daac158826baa2d6dc56809248869d332c0518
Michel <59454429+ducter-dev@users.noreply.github.com>
2022-01-11 10:07:01 -0600

    :lipstick: Rediseño login. (#12)

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,59 +1,36 @@
 <template>
   <div class="container mx-auto h-full flex justify-center items-center">
     <div class="w-3/12">
-      <div class="grid grid-cols-1 gap-6">
-        <label class="block">
-          <span class="text-dark">Usuario</span>
-          <input
-            type="text"
+        <form>
+          <div class="mb-6">
+            <label for="user" class="block mb-2 text-base font-bold text-dark">Usuario</label>
+            <input type="text" id="user" class="bg-gray-50 border border-gray-300 text-dark text-base font-semibold rounded-lg focus:ring-black focus:border-dark block w-full p-2.5" placeholder="" required>
+          </div>
+          <div class="mb-6">
+            <label for="password" class="block mb-2 text-base font-bold text-dark">Contraseña</label>
+            <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-dark text-base font-semibold rounded-lg focus:ring-black focus:border-dark block w-full p-2.5" placeholder="" required>
+          </div>
+          <button
+            type="button"
             class="
-              mt-1
-              block
               w-full
-              rounded-md
-              border-dark
-              shadow-sm
-              focus:border-black focus:ring focus:dark focus:ring-opacity-50
+              text-white
+              bg-dark
+              hover:bg-black
+              focus:ring-4 focus:ring-dark
+              font-medium
+              rounded-lg
+              text-base
+              px-5
+              py-2.5
+              text-center
+              mr-2
+              mb-2
             "
-            placeholder=""
-          />
-        </label>
-        <label class="block">
-          <span class="text-dark">Contraseña</span>
-          <input
-            type="email"
-            class="
-              mt-1
-              block
-              w-full
-              rounded-md
-              border-dark
-              shadow-sm
-              focus:border-black focus:ring focus:dark focus:ring-opacity-50
-            "
-            placeholder="usuario@example.com"
-          />
-        </label>
-        <button
-          type="button"
-          class="
-            text-white
-            bg-dark
-            hover:bg-black
-            focus:ring-4 focus:ring-blue-300
-            font-medium
-            rounded-lg
-            text-sm
-            px-5
-            py-2.5
-            text-center
-            mr-2
-            mb-2
-          "
-        >
-          Iniciar Sesión
-        </button>
-      </div>
+          >
+            Iniciar Sesión
+          </button>
+        </form>
     </div>
   </div>
 </template>
