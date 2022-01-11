ee4c691ab38c2c983b9151cc5338ab006a3d78d8
Michel <59454429+ducter-dev@users.noreply.github.com>
2022-01-11 10:34:17 -0600

    :lipstick: Maquetando dashboard (#15)

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from './views/Login.vue'
+import Dashboard from './views/Dashboard.vue'
 
 const routes = [
-  { path: '/', component: Login }
+  { path: '/', component: Login },
+  { path: '/dashboard', component: Dashboard },
 ]
 
 const history = createWebHistory()
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="mx-auto h-full flex justify-center items-center">
+    <div class="w-32 h-full bg-dark">SideBar</div>
+    <div class="w-11/12 flex-col items-start h-full">
+      <div class="h-10 m-4 bg-info">NavBar</div>
+      <div class="h-full m-4 bg-green-200">Contenido</div>
+    </div>
+  </div>
+</template>
