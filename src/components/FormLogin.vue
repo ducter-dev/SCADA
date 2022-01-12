057d1b56ac85b15dca500cd8dc7f44605b5da14a
Michel <59454429+ducter-dev@users.noreply.github.com>
2022-01-12 10:04:50 -0600

    :sparkles: Se agregó función para enviar datos de usuario a loguearse. (#19)

---
+++ src/components/FormLogin.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="w-3/12">
+    <form @submit.prevent="onSubmit">
+      <div class="mb-6">
+        <label for="user" class="block mb-2 text-base font-bold text-dark"
+          >Usuario</label
+        >
+        <input
+          type="text"
+          v-model="userForm.usuario"
+          class="
+            bg-gray-50
+            border border-gray-300
+            text-dark text-base
+            font-semibold
+            rounded-lg
+            focus:ring-black focus:border-dark
+            block
+            w-full
+            p-2.5
+          "
+          placeholder=""
+          required
+        />
+      </div>
+      <div class="mb-6">
+        <label for="password" class="block mb-2 text-base font-bold text-dark"
+          >Contraseña</label
+        >
+        <input
+          type="password"
+          v-model="userForm.password"
+          class="
+            bg-gray-50
+            border border-gray-300
+            text-dark text-base
+            font-semibold
+            rounded-lg
+            focus:ring-black focus:border-dark
+            block
+            w-full
+            p-2.5
+          "
+          placeholder=""
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        class="
+          w-full
+          text-white
+          bg-dark
+          hover:bg-black
+          focus:ring-4 focus:ring-dark
+          font-medium
+          rounded-lg
+          text-base
+          px-5
+          py-2.5
+          text-center
+          mr-2
+          mb-2
+        "
+      >
+        Iniciar Sesión
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const userForm = ref({
+      usuario: '',
+      password: '',
+    })
+
+    const onSubmit = async() => {
+      console.log(userForm.value)
+    }
+
+    return {
+      userForm,
+      onSubmit, 
+    }
+  }
+}
+
+
+
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,36 +1,9 @@
 <template>
   <div class="container mx-auto h-full flex justify-center items-center">
-    <div class="w-3/12">
-        <form>
-          <div class="mb-6">
-            <label for="user" class="block mb-2 text-base font-bold text-dark">Usuario</label>
-            <input type="text" id="user" class="bg-gray-50 border border-gray-300 text-dark text-base font-semibold rounded-lg focus:ring-black focus:border-dark block w-full p-2.5" placeholder="" required>
-          </div>
-          <div class="mb-6">
-            <label for="password" class="block mb-2 text-base font-bold text-dark">Contraseña</label>
-            <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-dark text-base font-semibold rounded-lg focus:ring-black focus:border-dark block w-full p-2.5" placeholder="" required>
-          </div>
-          <button
-            type="button"
-            class="
-              w-full
-              text-white
-              bg-dark
-              hover:bg-black
-              focus:ring-4 focus:ring-dark
-              font-medium
-              rounded-lg
-              text-base
-              px-5
-              py-2.5
-              text-center
-              mr-2
-              mb-2
-            "
-          >
-            Iniciar Sesión
-          </button>
-        </form>
-    </div>
+    <FormLogin />
   </div>
 </template>
+
+<script setup>
+import FormLogin from '../components/FormLogin.vue'
+</script>
