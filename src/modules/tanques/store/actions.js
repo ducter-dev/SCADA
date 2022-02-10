5d03bb75b16750617a8e33b26ebc4cdf0c374b9b
Michel <59454429+ducter-dev@users.noreply.github.com>
2022-02-10 11:27:33 -0600

    :zap: Agregados action y mutations de tanques. (#54)

--- src/modules/tanques/store/actions.js
+++ src/modules/tanques/store/actions.js
@@ -4,11 +4,28 @@ export const getTanks = async ({commit}) => {
   try {
     console.log('action => getTanks')
     const res = await scadaApi.get('/tanques')
-    console.log(res)
     const { data } = res
     commit('addTanks', data)
     return res
   } catch (error) {
     return { ok: false, message: error.message }
+  }
+}
+
+export const editTank = async ({ commit }, tank) => {
+  console.log('action => editTank')
+  try {
+    commit('updateTank', tank)
+  } catch (error) {
+    
+  }
+}
+
+export const deleteTank = async ({ commit }, tank) => {
+  console.log('action => deleteTank')
+  try {
+    commit('deleteTank', tank)
+  } catch (error) {
+    
   }
 }
