326061e9b3d9fc4c06d6b30001e67afa8140406c
Michel <59454429+ducter-dev@users.noreply.github.com>
2022-02-11 13:02:51 -0600

    :sparkles: Espacios para Dashboard (#55)
    Co-authored-by: DucterEduardo <desarrollador.jr@ducter.com.mx>

--- src/modules/dashboard/views/Dashboard.vue
+++ src/modules/dashboard/views/Dashboard.vue
@@ -1,7 +1,45 @@
 <template>
-  <div class="flex flex-col">
-    <div class="w-full flex flex-row justify-between items-center h-20">
-      <span>Dashcboard</span>
+  <div class="m-2 sm:mx-12 sm:my-2 w-full flex flex-col">
+    <div class="flex flex-row justify-center items-start border border-red-500">
+      <div class="flex justity-center items center w-3/12 border border-blue-900">
+        Card 1
+      </div>
+      <div class="flex justity-center items center w-3/12 border border-blue-900">
+        Card 2
+      </div>
+      <div class="flex justity-center items center w-3/12 border border-blue-900">
+        Card 3
+      </div>
+      <div class="flex justity-center items center w-3/12 border border-blue-900">
+        Card 4
+      </div>
+    </div>
+    <div class="flex flex-row justify-center items-start border border-red-500">
+      <div class="flex justity-center items center w-3/12 border border-blue-900">
+        Card 1
+      </div>
+      <div class="flex justity-center items center w-3/12 border border-blue-900">
+        Card 2
+      </div>
+      <div class="flex justity-center items center w-3/12 border border-blue-900">
+        Card 3
+      </div>
+      <div class="flex justity-center items center w-3/12 border border-blue-900">
+        Card 4
+      </div>
+    </div>
+    <div class="flex flex-row justify-center items-start border border-red-500">
+      Lista de Espera
     </div>
   </div>
 </template>
+
+<script>
+
+export default {
+  setup() {
+    return {
+    }
+  },
+};
+</script>
