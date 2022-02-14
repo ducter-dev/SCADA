c5176c9a353213056baf408371afb4200d8f73f2
DucterEduardo <desarrollador.jr@ducter.com.mx>
2022-02-14 11:53:06 -0600

    :sparkles: Maquetado Antena Verificacion

---
+++ src/modules/dashboard/components/TarjetaVerificacion.vue
@@ -0,0 +1,67 @@
+<template>
+  <div 
+  class="
+      p-2
+      m-2
+      max-w-sm
+      bg-white
+      rounded-lg
+      border border-gray-200
+      shadow-md
+      flex flex-col
+    "
+  >
+    <h5 class="
+        flex
+        justify-center
+        items-center
+        text-gray-400 text-xl
+        font-bold
+        mt-1
+        mb-1
+      ">
+      Antena Verificación
+    </h5>
+    <div class="flex flex-row justify-around items-start">
+      <span class="
+        flex
+        justify-center
+        items-center
+        text-gray-400 text-sm
+        font-bold
+        mt-1
+        mb-1
+      ">Num AT. </span>
+      <span  class="
+        flex
+        justify-center
+        items-center
+        text-gray-700 text-sm
+        font-bold
+        mt-1
+        mb-1
+      "> 1544</span>
+    </div>
+      <p></p>
+    <div class="flex flex-row justify-around items-start">
+      <span class="
+        flex
+        justify-center
+        items-center
+        text-gray-400 text-sm
+        font-bold
+        mt-1
+        mb-1
+      ">Tipo AT: </span>
+      <span  class="
+        flex
+        justify-center
+        items-center
+        text-gray-700 text-sm
+        font-bold
+        mt-1
+        mb-1
+      "> Ninguno</span>
+    </div>
+  </div>
+</template>
