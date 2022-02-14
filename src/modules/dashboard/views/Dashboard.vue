48b3eed47b5e858e30455849c5cd95e3c9ec5a9a
DucterEduardo <60186839+DucterEduardo@users.noreply.github.com>
2022-02-14 11:40:30 -0600

    :sparkles: Maquetado Tarjeta Entrada (#63)

--- src/modules/dashboard/views/Dashboard.vue
+++ src/modules/dashboard/views/Dashboard.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="m-2 sm:mx-12 sm:my-2 w-full flex flex-col">
-    <div class="flex flex-row justify-center items-start border border-red-500">
-      <div class="flex justity-center items center w-3/12 border border-blue-900">
-        Card 1
+    <div class="flex flex-row justify-center items-start">
+      <div class="flex justify-center items center w-3/12">
+        <TarjetaEntrada />
       </div>
-      <div class="flex justity-center items center w-3/12 border border-blue-900">
+      <div class="flex justify-center items center w-3/12 border border-blue-900">
         Card 2
       </div>
-      <div class="flex justity-center items center w-3/12 border border-blue-900">
+      <div class="flex justify-center items center w-3/12 border border-blue-900">
         Card 3
       </div>
-      <div class="flex justity-center items center w-3/12 border border-blue-900">
+      <div class="flex justify-center items center w-3/12 border border-blue-900">
         Card 4
       </div>
     </div>
     <div class="flex flex-row justify-center items-start border border-red-500">
-      <div class="flex justity-center items center w-3/12 border border-blue-900">
-        Card 1
+      <div class="flex justify-center items center w-3/12 border border-blue-900">
+        Card 5
       </div>
-      <div class="flex justity-center items center w-3/12 border border-blue-900">
-        Card 2
+      <div class="flex justify-center items center w-3/12 border border-blue-900">
+        Card 6
       </div>
-      <div class="flex justity-center items center w-3/12 border border-blue-900">
-        Card 3
+      <div class="flex justify-center items center w-3/12 border border-blue-900">
+        Card 7
       </div>
-      <div class="flex justity-center items center w-3/12 border border-blue-900">
-        Card 4
+      <div class="flex justify-center items center w-3/12 border border-blue-900">
+        Card 8
       </div>
     </div>
     <div class="flex flex-row justify-center items-start border border-red-500">
@@ -35,8 +35,12 @@
 </template>
 
 <script>
+import TarjetaEntrada from '../components/TarjetaEntrada.vue'
 
 export default {
+  components: {
+    TarjetaEntrada
+  },
   setup() {
     return {
     }
