d6f2b99a363d41d1e650bb2365aac6b935a1e3e5
EfrenColinDC <125599016+EfrenColinDC@users.noreply.github.com>
2023-03-02 12:09:17 -0600

    SIN-78 se agrego la vista de servicios (#142)

--- src/layout/components/Card/CardListItem.vue
+++ src/layout/components/Card/CardListItem.vue
@@ -5,8 +5,15 @@ defineProps({
         default:'Label'
     },
     value:{
+        require:false,
+    },
+    labelClass:{
         type:String,
-        default:'Value'
+        require:false
+    },
+    valueClass:{
+        type:String,
+        require:false
     },
     Lname: {
         type: String,
@@ -18,9 +25,12 @@ defineProps({
     <li class="py-1">
         <div class="flex items-center space-x-4">
             <div class="flex-1 min-w-0">
-                <p class="text-sm font-medium truncate text-slate-700 dark:text-slate-300" v-html="label"></p>
+                <p class="text-sm font-medium truncate text-slate-700 dark:text-slate-300" v-html="label" :class="labelClass"></p>
             </div>
-            <div class="inline-flex items-center text-sm font-semibold text-slate-900 dark:text-white" v-html="value"></div>
+            <div class="inline-flex items-center text-sm font-semibold text-slate-900 dark:text-white" :class="valueClass">
+                {{ value }}
+                <slot/>
+            </div>
         </div>
     </li>
 </template>
