2623584a8593e5ea1897f6ef1745b5efdfd86c2e
Hector Lopez <hector.lopez@ducter.com.mx>
2023-03-16 12:50:28 -0600

    sin-96 se agrego la seccion de configuracion

--- src/modules/dashboard/views/Configuracion.vue
+++ src/modules/dashboard/views/Configuracion.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="m-4 sm:m-12 w-full flex flex-col">
-    <!-- <Usuarios /> -->
+    <Usuarios /> 
     <Tanques />
   </div>
 </template>
 
 <script>
-/* import Usuarios from '../../usuarios/views/Usuarios.vue' */
+import Usuarios from '../../usuarios/views/Usuarios.vue'
 import Tanques from '../../tanques/views/Tanques.vue'
 export default {
   components: {
-    /* Usuarios, */
+    Usuarios,
     Tanques,
   },
   setup () {
