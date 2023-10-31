78c55087ef058926d576c288499bbecd0eb03fe8
ducterdev <michel.morales@ducter.com.mx>
2023-10-31 13:31:56 -0600

    :sparkles: Actualización de tanque desde lista de espera

--- src/modules/dashboard/components/LastEntry.vue
+++ src/modules/dashboard/components/LastEntry.vue
@@ -81,8 +81,6 @@ const fetchDataLastEntry = async () => {
     }
 }
 
-
-
 onMounted(() => {
     fetchDataLastEntry()
 })
@@ -110,32 +108,4 @@ onMounted(() => {
             </ul>
         </div>
     </div>
-    <!-- <div class="max-w-sm mt-5 bg-transparent">
-        <div class="p-2 border border-solid border-slate-300">
-            <div class="py-1">
-                <div class="flex items-center space-x-4">
-                    <div class="flex-1 min-w-0">
-                        <p class="text-sm font-medium truncate text-slate-700 dark:text-slate-300">Barrera de entrada
-                        </p>
-                    </div>
-
-                    <div class="flex items-center justify-center mx-2">
-                        <Toggle v-model="dataBarrierEntryStatus" offLabel="Cerrada" onLabel="Abierta" :classes="{
-                            toggle: 'flex w-[5.5rem] py-2 relative cursor-pointer transition items-center box-content text-sm leading-none',
-                            toggleOn: 'bg-green-200 dark:bg-green-700 border-salte-300 dark:border-salte-800 justify-start text-green-900 dark:text-white',
-                            toggleOff: 'bg-red-200 dark:bg-red-700 border-salte-300 dark:border-salte-800 justify-end text-red-900 dark:text-white',
-                            toggleOnDisabled: 'bg-slate-200 border-slate-200 dark:bg-slate-700 dark:border-slate-800 justify-start text-slate-400 dark:text-slate-400 cursor-not-allowed',
-                            toggleOffDisabled: 'bg-slate-200 border-slate-200 dark:bg-slate-700 dark:border-slate-800 justify-end text-slate-400 dark:text-slate-400 cursor-not-allowed',
-                            handle: 'inline-block bg-white dark:bg-slate-600 w-5 h-5 p-1 mx-1 top-1 absolute transition-all',
-                            handleOn: 'left-20 transform -translate-x-full',
-                            handleOff: 'left-0',
-                            handleOnDisabled: 'bg-slate-100  left-20 transform -translate-x-full',
-                            handleOffDisabled: 'bg-slate-100  left-0',
-                            label: 'text-center border-box whitespace-nowrap select-none mx-2',
-                        }" />
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
 </template>
